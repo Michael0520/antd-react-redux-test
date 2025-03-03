922cd1d45eded1fdc84e9bf603f3f0f9ac79a9cb
Michael0520 <michael860520@gmail.com>
2025-03-04 03:32:38 +0800

    feat: update app root with class router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { ConfigProvider } from 'antd'
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import ClassList from './pages/class'
+import ClassDetail from './pages/class/[classId]'
+import { store } from './store'
 
-import '/@/styles/global.css'
-import { Router } from '/@/router'
-import { store } from '/@/store'
+const App: React.FC = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/class/index" replace />} />
+        <Route path="/class">
+          <Route path="index" element={<ClassList />} />
+          <Route path=":classId" element={<ClassDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+)
 
-import { formConfig, getPopupContainerConfig } from '/@/utils/ant'
-
-export function App() {
-  return (
-    <ConfigProvider form={formConfig} getPopupContainer={getPopupContainerConfig}>
-      <Provider store={store}>
-        <Router />
-      </Provider>
-    </ConfigProvider>
-  )
-}
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-import { App } from '/@/App'
+import App from './App'
 
 const rootElement = document.querySelector('#root')!
+const root = createRoot(rootElement)
 
-const app = createRoot(rootElement!)
-
-app.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
